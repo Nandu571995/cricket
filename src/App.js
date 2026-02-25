b91423a418b9484f1d5699127dece0f89dbd71de
Nandu571995 <nandkishor10000008@gmail.com>
2026-02-25 19:12:07 +0530

    Create src/App.js as main React component for cricket overlay

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Cricket Overlay Component</h1>
+            {/* Add your overlay content here */}
+        </div>
+    );
+};
+
+export default App;
